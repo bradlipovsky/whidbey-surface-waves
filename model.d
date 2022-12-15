b1247f5ccde3c905354427ba2ffe75a9801ca614
Brad Lipovsky <bradlipovsky@gmail.com>
2022-12-15 11:36:44 -0600

    added more obs

--- model.d
+++ model.d
@@ -1,16 +1,16 @@
-MODEL 
-TEST MODEL 
-ISOTROPIC 
-KGS 
-FLAT EARTH 
-1-D 
-CONSTANT VELOCITY 
-LINE08 
-LINE09 
-LINE10 
+MODEL.01
+Model after     5 iterations
+ISOTROPIC
+KGS
+FLAT EARTH
+1-D
+CONSTANT VELOCITY
+LINE08
+LINE09
+LINE10
 LINE11
-HR    VP    VS    RHO   QP  QS  ETAP ETAS FREFP FREFS
-0.100 1.500 0.000 1.000 0.0 0.0 0.0  0.0  1.0   1.0
-0.020 1.600 0.045 1.500 0.0 0.0 0.0  0.0  1.0   1.0
-0.020 1.250 0.100 1.500 0.0 0.0 0.0  0.0  1.0   1.0
-0.000 1.600 4.300 1.500 0.0 0.0 0.0  0.0  1.0   1.0
+      H(KM)   VP(KM/S)   VS(KM/S) RHO(GM/CC)     QP         QS       ETAP       ETAS      FREFP      FREFS    
+     0.1000     1.5000     0.0000     1.0000   0.00       0.00       0.00       0.00       1.00       1.00    
+     0.0200     1.4562     0.0410     1.1903   0.00       0.00       0.00       0.00       1.00       1.00    
+     0.0200     1.4612     0.1169     1.5543   0.00       0.00       0.00       0.00       1.00       1.00    
+     0.0000     2.8382     1.6395     2.1131   0.00       0.00       0.00       0.00       1.00       1.00    
