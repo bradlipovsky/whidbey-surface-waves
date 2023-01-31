5ffe9a88a334f504b3f2b1844106a92e115d4b67
Brad Lipovsky <bradlipovsky@gmail.com>
2023-01-30 20:27:38 -0800

    added more

--- inversion.sh
+++ inversion.sh
@@ -11,12 +11,12 @@ echo "--->  Differential smoothing"
 surf96 36 1
 
 echo "--->  Set up five iterations"
-#surf96 1 2 6 1 2
-surf96 1 2 6 1 2 6 1 2 6 1 2 6 1 2 6 1 2
+surf96 1 2 6 1 2 6 1 2
+#surf96 1 2 6 1 2 6 1 2 6 1 2 6 1 2 6 1 2
 
 echo "--->  Make plots"
-srfphv96
-plotnps -EPS -K -F7 -W10 < SRFPHV96.PLT > figsrf1.eps
+#srfphv96
+#plotnps -EPS -K -F7 -W10 < SRFPHV96.PLT > figsrf1.eps
 
 echo "---> Save"
 surf96 28 modl.out
