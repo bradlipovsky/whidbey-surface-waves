c76bc88cfe590e9b65ad6ce4a3faee04a6ec6e6a
Brad Lipovsky <bradlipovsky@gmail.com>
2023-02-13 15:47:11 -0800

    worked on better mcmc plots

--- scholte.py
+++ scholte.py
@@ -37,7 +37,10 @@ def pysurf96(hr,vp,vs,rho,model_file='model-from-python.d'):
     k = data['FREQ']/data['PHASE VEL']/1e3
     return f,k
 
-def wrap_four_layer(p,h,k_obs,vp = [1.5,1.6,1.6,1.6,1.6], rho= [1,1.5,1.5,1.5,1.5]):
+def wrap_four_layer(p,h,k_obs,
+                        vp = [1.5,1.6,1.6,1.6,1.6], 
+                        rho= [1,1.5,1.5,1.5,1.5]  ):
+
     this_vs = [0,p[0],p[1],p[2],p[3]]
     f,k = pysurf96(h,vp,this_vs,rho)
     misfit = np.abs(k[3:]-k_obs).sum()
@@ -71,7 +74,12 @@ def do_mcmc_4_param():
     with open('100k_4param_MCMC.pickle', 'wb') as handle:
         pickle.dump(x_list, handle, protocol=pickle.HIGHEST_PROTOCOL)
     
-def do_mcmc_8param(kobs, N = 200000, filename='100k_4param_MCMC.pickle',fixhalfspace=True):
+def do_mcmc_8param(kobs, 
+                   N = 200000, 
+                   filename='100k_4param_MCMC.pickle',
+                   fixhalfspace=True,
+                   restartfile=None):
+    
     t0 = perf_counter()
 
     step_size = 1.0e-4 # 0.1 m/s
@@ -81,7 +89,6 @@ def do_mcmc_8param(kobs, N = 200000, filename='100k_4param_MCMC.pickle',fixhalfs
     x0 = [0.038,0.043,0.080,0.60]
     h0 = [0.1,0.005,0.010,0.020,0.0]
     m0 = wrap_four_layer(x0,h0,kobs)
-    
 
     x_list = []
     for i in tqdm(range(N)):
@@ -101,8 +108,50 @@ def do_mcmc_8param(kobs, N = 200000, filename='100k_4param_MCMC.pickle',fixhalfs
     x_list = np.array(x_list) * 1e3
     print(f'Runtime was {perf_counter()-t0}')
     
+    if restartfile is not None:
+        x_list_old = pickle.load(open(restartfile, 'rb'))
+        x_list = np.vstack((x_list_old,x_list))
+    
     with open(filename, 'wb') as handle:
         pickle.dump(x_list, handle, protocol=pickle.HIGHEST_PROTOCOL)
     
 def exp_vs(z,z0,exp,coeff):
     return (coeff*(z - z0))**exp
+
+
+def plt_dispersion(f,k,fobs,kobs,z,vs,save=False):
+
+    fig,ax=plt.subplots(1,2,figsize=(16,9))
+    fig.patch.set_facecolor('w')
+    plt.subplot(121)
+    plt.step(vs,z,label='Vs (km/s)')
+    # plt.step(p,z,label='Vp (km/s)')
+    # plt.step(rho,z,'--',label='Density (kg/m**3)')
+    plt.ylim([max(z),0])
+    plt.grid()
+    # plt.legend(fontsize=18)
+    plt.ylabel('Depth (m)',fontsize=22)
+    plt.xlabel('Shear Wave Speed (m/s)',fontsize=22)
+    plt.text(0.1,50,'water',fontsize=18)
+    plt.text(100,120,'fluidized \nsediments',fontsize=18)
+    plt.text(400,175,'   compact \n sediments',fontsize=18)
+
+    plt.xlim([0, 650])
+
+    plt.subplot(122)
+    plt.plot(k,f,'-o',label='Model',markersize=12)
+    plt.plot( kobs , fobs, 'o' , label='Observation',markersize=10)
+    plt.ylabel('Frequency (Hz)',fontsize=22)
+    plt.xlabel(r'Ordinary Wavenumber, $1/\lambda$ (1/m)',fontsize=22)
+
+    
+    fit = linregress(kobs,fobs)
+    plt.plot(kobs, fit.slope*kobs + fit.intercept,'--',label=f'Constant vg ({fit.slope:.0f} m/s)')
+    print(fit.slope)
+    print(fit.intercept)
+    plt.legend(fontsize=18)
+    plt.grid()
+    if save:
+        plt.savefig('model.eps')
+    else:
+        plt.show()
