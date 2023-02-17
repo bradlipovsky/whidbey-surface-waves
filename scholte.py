e09a47def7f664bfa0edd61657b04773d686aa9e
Brad Lipovsky <bradlipovsky@gmail.com>
2023-02-17 12:12:38 -0800

    fixed mcmc logic bugs

--- scholte.py
+++ scholte.py
@@ -7,6 +7,7 @@ import subprocess
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
+from scipy.interpolate import interp1d
 
 def pysurf96(hr,vp,vs,rho,model_file='model-from-python.d',N0 = 18, N1 = 15):
 
@@ -56,74 +57,69 @@ def pysurf96(hr,vp,vs,rho,model_file='model-from-python.d',N0 = 18, N1 = 15):
     k.append( np.array(k_this_mode) )
     return f,k
 
-def wrap_four_layer(p,h,k_obs,
+def wrap_four_layer(p,h,k_obs, f_obs,
                         vp = [1.5,1.6,1.6,1.6,1.6], 
                         rho= [1,1.5,1.5,1.5,1.5]  ):
 
     this_vs = [0,p[0],p[1],p[2],p[3]]
     f,k = pysurf96(h,vp,this_vs,rho)
-    misfit = np.abs(k[3:]-k_obs).sum()
+    
+    misfit = 0
+    for fm_mode,km_mode,fo_mode,ko_mode in zip(f,k,f_obs,k_obs):
+        # Loop over the different Scholte wave modes
+        if len(km_mode) < 2:
+            # surf96 failed?
+            return np.Inf
+        f_interp = interp1d(km_mode,fm_mode)
+        # index to account for the fact that surf96 may not have found
+        # the dispersion curve where we have observations -- only analyze
+        # where the model and observations overlap.
+        index = (min(km_mode) < ko_mode) & (ko_mode < max(km_mode))
+        f_model = f_interp(ko_mode[index])
+        misfit = misfit + np.abs(f_model - fo_mode[index]).sum()
 
     return misfit
-
-def do_mcmc_4_param():
+    
+def do_mcmc_8param(kobs, fobs, h0, x0,
+                    N = 200000, 
+                    filename='100k_4param_MCMC.pickle',
+                    fixhalfspace=True,
+                    fixdepths=True,
+                    restartfile=None,
+                    step_size = 1.0e-4, # 0.1 m/s
+                    step_size_h = 0.01e-3, # 0.01 m
+                    f_sensitivity = 1e-3
+                  ):
+    
     t0 = perf_counter()
-
-    step_size = 2.5e-4 # 0.25 m/s
-    # k_sensitivity = 0.000418 * len(kobs)
-    k_sensitivity = 7.5e-5
-    x0 = [0.038,0.043,0.080,0.60]
-    h0 = [0.1,0.005,0.010,0.020,0.0]
-    misfit0 = wrap_four_layer(x0,h0,kobs)
-    N = 100000
+    
+    misfit0 = wrap_four_layer(x0,h0,kobs,fobs)
+    L0 =  -misfit0**2 / f_sensitivity**2
 
     x_list = []
     for i in tqdm(range(N)):
-        x = x0 + step_size*(2*random_sample(len(x0))-1)
-        misfit = wrap_four_layer(x,h0,kobs)
-        L =  np.exp(-(misfit-misfit0)**2 / k_sensitivity**2)
-        thr = random_sample()
-        if L >= thr:
-            x_list.append(x)
-            x0 = x
-            m0 = m
-    x_list = np.array(x_list) * 1e3
-    print(f'Runtime was {perf_counter()-t0}')
-    
-    with open('100k_4param_MCMC.pickle', 'wb') as handle:
-        pickle.dump(x_list, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    
-def do_mcmc_8param(kobs, 
-                   N = 200000, 
-                   filename='100k_4param_MCMC.pickle',
-                   fixhalfspace=True,
-                   restartfile=None):
-    
-    t0 = perf_counter()
-
-    step_size = 1.0e-4 # 0.1 m/s
-    step_size_h = 0.01e-3 # 0.01 m
-    # k_sensitivity = 0.000418 * len(kobs)
-    k_sensitivity = 5e-5
-    x0 = [0.038,0.043,0.080,0.60]
-    h0 = [0.1,0.005,0.010,0.020,0.0]
-    m0 = wrap_four_layer(x0,h0,kobs)
-
-    x_list = []
-    for i in tqdm(range(N)):
-        x = x0 + step_size*(2*random_sample(len(x0))-1)
+#         x = x0 + step_size*(2*random_sample(len(x0))-1)
+        x = x0 + np.random.default_rng().normal(0, step_size, len(x0))
+        
         if fixhalfspace:
             x[3]=x0[3]
-        h = h0 + step_size_h*(2*random_sample(len(h0))-1)
-        h[-1]=0
-        m = wrap_four_layer(x,h,kobs)
-        L =  np.exp(-(m-m0)**2 / k_sensitivity**2)
-        thr = random_sample()
-        if L >= thr:
+            
+        if not fixdepths:
+            return np.Nan # not implemented yet
+            h = h0 + step_size_h*(2*random_sample(len(h0))-1)
+            h[-1]=0
+        else:
+            h=h0
+            
+        misfit = wrap_four_layer(x,h,kobs,fobs)
+        L =  -misfit**2 / f_sensitivity**2
+        alpha = np.exp(L-L0)
+        u = random_sample()
+        if alpha > u: # accept the proposition
             x_list.append( np.hstack((h0,x0)) )
             x0 = x
             h0 = h
-            m0 = m
+            misfit0 = misfit
     x_list = np.array(x_list) * 1e3
     print(f'Runtime was {perf_counter()-t0}')
     
@@ -138,45 +134,19 @@ def exp_vs(z,z0,exp,coeff):
     return (coeff*(z - z0))**exp
 
 
-def plt_dispersion(f,k,fobs,kobs,z,vs,save=False):
 
-    fig,ax=plt.subplots(1,2,figsize=(16,9))
-    fig.patch.set_facecolor('w')
-    plt.subplot(121)
-    plt.step(vs,z,label='Vs (km/s)')
-    # plt.step(p,z,label='Vp (km/s)')
-    # plt.step(rho,z,'--',label='Density (kg/m**3)')
-    plt.ylim([max(z),0])
-    plt.grid()
-    # plt.legend(fontsize=18)
-    plt.ylabel('Depth (m)',fontsize=22)
-    plt.xlabel('Shear Wave Speed (m/s)',fontsize=22)
-    plt.text(0.1,50,'water',fontsize=18)
-    plt.text(100,120,'fluidized \nsediments',fontsize=18)
-    plt.text(400,175,'   compact \n sediments',fontsize=18)
+        
+def load_dispersion_observations():
+    kobs0_manual = np.array([0.04, 0.035, 0.030, 0.025, 0.020, 0.0150, 0.0113, 0.0059, 0.0029, 0.00042])
+    fobs0_manual = np.array([1.43, 1.35, 1.250, 1.140, 1.02, 0.88, 0.77, 0.585, 0.46, 0.378])
 
-    plt.xlim([0, 650])
+    kobs1_manual = np.array([0.001, 0.002, 0.003, 0.004, 0.005, 0.007, 0.009, 0.011, 0.013])
+    fobs1_manual = np.array([0.756,   0.80, 0.835, 0.865, 0.90, 0.95, 1.02, 1.09, 1.15])
 
-    plt.subplot(122)
-    ind = 0
-    for kk,ff in zip(k,f):
-        plt.plot(kk,ff,'-o',label=f'Model Mode {ind}',markersize=12)
-        ind = ind+1
-    ind = 0
-    for kk,ff in zip(kobs,fobs):
-        plt.plot( kk , ff, 'o' , label=f'Obs. Mode {ind}',markersize=10)
-        ind = ind+1
-    plt.ylabel('Frequency (Hz)',fontsize=22)
-    plt.xlabel(r'Ordinary Wavenumber, $1/\lambda$ (1/m)',fontsize=22)
+    kobs2_manual = np.array([0.002,0.003,0.004,0.006,0.008,0.010,0.012,0.014])
+    fobs2_manual = np.array([1.10,1.21,1.26,1.35,1.45,1.55,1.65,1.75])
 
+    kobs = [kobs0_manual,kobs1_manual,kobs2_manual]
+    fobs = [fobs0_manual,fobs1_manual,fobs2_manual]
     
-#     fit = linregress(kobs,fobs)
-#     plt.plot(kobs, fit.slope*kobs + fit.intercept,'--',label=f'Constant vg ({fit.slope:.0f} m/s)')
-#     print(fit.slope)
-#     print(fit.intercept)
-    plt.legend(fontsize=18)
-    plt.grid()
-    if save:
-        plt.savefig('model.eps')
-    else:
-        plt.show()
+    return kobs,fobs
